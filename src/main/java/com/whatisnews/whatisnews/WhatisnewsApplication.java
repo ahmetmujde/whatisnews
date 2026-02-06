8bd3d3c1e1489606f0460aa0a3f16098c4f2927b
ahmetmujde <mujde.ahmet79@gmail.com>
2026-02-06 14:22:03 +0300

    Initial commit

---
+++ src/main/java/com/whatisnews/whatisnews/WhatisnewsApplication.java
@@ -0,0 +1,13 @@
+package com.whatisnews.whatisnews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatisnewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatisnewsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/whatisnews/whatisnews/WhatisnewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whatisnews.whatisnews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatisnewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
